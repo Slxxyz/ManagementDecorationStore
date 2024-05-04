16d1c96ed1a6399155386e7f236a7e4f682f4a96
Evren Konakci <142110440+Slxxyz@users.noreply.github.com>
2024-05-04 12:13:38 +0200

    Getters & Setters ADDED

--- model/Supplier.java
+++ model/Supplier.java
@@ -16,4 +16,56 @@ public class Supplier {
         this.bankingInformation = bankingInformation;
         this.vatNumber = vatNumber;
     }
+    //////////////////////////////////////////////////////////////////GETTERS
+
+    public String getLegalName() {
+        return legalName;
+    }
+
+    public String getStreetAndNumber() {
+        return streetAndNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public String getBankingInformation() {
+        return bankingInformation;
+    }
+
+    public String getVatNumber() {
+        return vatNumber;
+    }
+
+    //////////////////////////////////////////////////////////////////SETTERS
+
+    public void setLegalName(String legalName) {
+        this.legalName = legalName;
+    }
+
+    public void setStreetAndNumber(String streetAndNumber) {
+        this.streetAndNumber = streetAndNumber;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setBankingInformation(String bankingInformation) {
+        this.bankingInformation = bankingInformation;
+    }
+
+    public void setVatNumber(String vatNumber) {
+        this.vatNumber = vatNumber;
+    }
+
 }
