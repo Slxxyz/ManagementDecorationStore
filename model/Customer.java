3e3be41cedbb3bfc5f1ec4b178d77d3975cca388
Evren Konakci <142110440+Slxxyz@users.noreply.github.com>
2024-05-04 12:09:11 +0200

    Getters & Setters ADDED

--- model/Customer.java
+++ model/Customer.java
@@ -25,5 +25,87 @@ public class Customer {
         this.birthday = birthday;
         this.isMarried = isMarried;
     }
+
+    //////////////////////////////////////////////////////////////////GETTERS
+    public int getNumber() {
+        return number;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public int getNbPoints() {
+        return nbPoints;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public Boolean getIsMarried() {
+        return isMarried;
+    }
+    //////////////////////////////////////////////////////////////////SETTERS
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    public void setNbPoints(int nbPoints) {
+        this.nbPoints = nbPoints;
+    }
+
+    public void setTelNumber(String telNumber) {
+        if(telNumber.isBlank() || telNumber.isEmpty()) {
+            this.telNumber = null;
+        } else {
+            this.telNumber = telNumber;
+        }
+    }
+
+    public void setMailAddress(String mailAddress) {
+        if(mailAddress.isBlank() || mailAddress.isEmpty()) {
+            this.mailAddress = null;
+        } else {
+            this.mailAddress = mailAddress;
+        }
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setIsMarried(Boolean isMarried) {
+        this.isMarried = isMarried;
+    }
+
 }
 
