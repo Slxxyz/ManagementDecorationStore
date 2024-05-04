f2b7acf10cb1d48bd8bc60c616ffb91b195e821e
Evren Konakci <142110440+Slxxyz@users.noreply.github.com>
2024-05-04 12:12:42 +0200

    Getters & Setters ADDED

--- model/Product.java
+++ model/Product.java
@@ -18,5 +18,65 @@ public class Product {
         this.isFragile = isFragile;
         this.loyaltyPointsValue = loyaltyPointsValue;
     }
+    //////////////////////////////////////////////////////////////////GETTERS
+    public int getCode() {
+        return code;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double getUnitPriceExcludingTax() {
+        return unitPriceExcludingTax;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public int getVATRate() {
+        return vATRate;
+    }
+
+    public boolean getIsFragile() {
+        return isFragile;
+    }
+
+    public int getLoyaltyPointsValue() {
+        return loyaltyPointsValue;
+    }
+
+    //////////////////////////////////////////////////////////////////SETTERS
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setUnitPriceExcludingTax(double unitPriceExcludingTax) {
+        this.unitPriceExcludingTax = unitPriceExcludingTax;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public void setVATRate(int vATRate) {
+        this.vATRate = vATRate;
+    }
+
+    public void setIsFragile(boolean isFragile) {
+        this.isFragile = isFragile;
+    }
+
+    public void setLoyaltyPointsValue(int loyaltyPointsValue) {
+        this.loyaltyPointsValue = loyaltyPointsValue;
+    }
+
+
 
 }
