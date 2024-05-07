dae25f000ec722687541bc14cfc7fe49466ab4d9
MatteoMameri <160240372+MatteoMameri@users.noreply.github.com>
2024-05-07 12:01:32 +0200

    Update OrderLineDataAccess.java

--- interfaceAccess/OrderLineDataAccess.java
+++ interfaceAccess/OrderLineDataAccess.java
@@ -6,4 +6,12 @@ import model.OrderLine;
 import java.util.ArrayList;
 
 public class OrderLineDataAccess {
+
+ void createOrderLine(OrderLine orderLine) throws OrderLineException 
+ 
+ ArrayList<OrderLine> readAllOrderLine() throws OrderLineException 
+      
+ ArrayList<OrderLine> readAllOrderLineFor(Integer orderLineCode) throws OrderLineException 
+
+  
 }
