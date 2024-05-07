061c3549606c6f864883eaf88825bcb613cdc883
MatteoMameri <160240372+MatteoMameri@users.noreply.github.com>
2024-05-07 12:11:05 +0200

    Update SupplierDetailDataAccess.java

--- interfaceAccess/SupplierDetailDataAccess.java
+++ interfaceAccess/SupplierDetailDataAccess.java
@@ -6,4 +6,13 @@ import model.SupplierDetail;
 import java.util.ArrayList;
 
 public class SupplierDetailDataAccess {
+
+    void createSupplierDetail(SupplierDetail supplierDetail) throws SupplierDetailException 
+
+    SupplierDetail readSupplierDetail(int productCode, String supplierName, LocalDate dateAndTimeId) throws SupplierDetailException 
+       
+    void updateSupplierDetail(SupplierDetail supplierDetail) throws SupplierDetailException 
+     
+
+
 }
