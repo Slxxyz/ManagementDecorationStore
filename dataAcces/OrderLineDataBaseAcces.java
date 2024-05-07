c4283f07b24bfaf4e5f3016425b269ed9d2d81c6
MatteoMameri <160240372+MatteoMameri@users.noreply.github.com>
2024-05-07 11:06:19 +0200

    Update OrderLineDataBaseAcces.java

--- dataAcces/OrderLineDataBaseAcces.java
+++ dataAcces/OrderLineDataBaseAcces.java
@@ -1,1 +1,89 @@
+package dataAcces;
+
+import exception.*;
+import interfaceAccess.OrderLine;
+import model.OrderLine;
+import java.sql.Date;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+
+
+
+public class OrderLine {
+
+
+
+    @Override
+    public void createOrderLine(OrderLine orderLine) throws OrderLineException {
+        try {
+            Connection connexion = SingletonConnexion.getInstance();
+            String query = "INSERT INTO orderLine VALUES (?,?,?,?);";
+            PreparedStatement statement = connexion.prepareStatement(query);
+            statement.setInt(1, orderLine.getCode());
+            statement.setInt(2, orderLine.getQuantity());
+            statement.setInt(3, orderLine.getOrderCustomer());
+            statement.setInt(4, orderLine.getProduct());
+            statement.executeUpdate();
+        } catch (SQLException exception) {
+            throw new OrderLineException(exception.getMessage(), new OneException(), new CreateException());
+        }
+    }
+
+
+    @Override// lis TOUTES les orderLine de la BD
+    public ArrayList<OrderLine> readAllOrderLine() throws OrderLineException {
+        try {
+            Connection connexion = SingletonConnexion.getInstance();
+            String query = "SELECT * FROM orderLine;";
+            PreparedStatement statement = connexion.prepareStatement(query);
+            ResultSet data = statement.executeQuery();
+            ArrayList<OrderLine> orderLines = new ArrayList<OrderLine>();
+            while (data.next()) {
+                int code = data.getInt("code");
+                Integer quantity = data.getInt("quantity");
+                Integer orderCustomer = data.getInt("orderCustomer");
+                Integer product= data.getInt("product");
+                OrderLine orderLine = new OrderLine(code,quantity,orderCustomer,product);
+                orderLines.add(orderLine);
+            }
+            return orderLines;
+        } catch (SQLException exception) {
+            throw new OrderLinesException(exception.getMessage(), new AllException(), new ReadException());
+        }
+    }
+
+
+
+
+
+
+    @Override //lis tte les order d'un Customer
+    public ArrayList<OrderLine> readAllOrderLineFor(Integer orderLineCode) throws OrderLineException {
+        try {
+            Connection connexion = SingletonConnexion.getInstance();
+            String query = "SELECT * FROM orderLine WHERE code = ?;";
+            PreparedStatement statement = connexion.prepareStatement(query);
+            statement.setInt(1, orderLineCode);
+            ResultSet data = statement.executeQuery();
+            ArrayList<OrderLine> orderLines = new ArrayList<OrderLine>();
+            while (data.next()) {
+                int code = data.getInt("code");
+                Integer quantity = data.getInt("quantity");
+                Integer orderCustomer = data.getInt("orderCustomer");
+                Integer product= data.getInt("product");
+                OrderLine orderLine = new OrderLine(code,quantity,orderCustomer,product);
+                orderLines.add(orderLine);
+            }
+            return orderLines;
+        } catch (SQLException exception) {
+            throw new OrderLineException(exception.getMessage(), new AllException(), new ReadException());
+        }
+    }
+
+
+
+
+}
 
