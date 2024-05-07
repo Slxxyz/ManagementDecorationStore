d2762ccbe0ade08c86cb83630b55889ff59d117b
MatteoMameri <160240372+MatteoMameri@users.noreply.github.com>
2024-05-07 11:56:03 +0200

    Update CustomerDataAccess.java

--- interfaceAccess/CustomerDataAccess.java
+++ interfaceAccess/CustomerDataAccess.java
@@ -7,6 +7,24 @@ import java.util.ArrayList;
 
 public class CustomerDataAccess {
 
+    void createCustomer(Customer customer) throws CustomerException 
+    
+    Customer readCustomer() throws CustomerException 
 
+   
+    void updateCustomer(Customer customer) throws CustomerException 
+      
+  
+    void deleteCustomer(int numberCustomer) throws CustomerException 
+
+   
+    ArrayList<Customer> readAllCustomer() throws CustomerException 
+     
+    
+    int getNextCode() throws NextCodeCustomerException 
+   
+    
+    int getNumberCustomer() throws NumberCustomerException 
+     
   
 }
