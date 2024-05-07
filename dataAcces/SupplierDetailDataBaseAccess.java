8d0dd1e0edad82f77d54e0b75a237f32ebb67296
Evren Konakci <142110440+Slxxyz@users.noreply.github.com>
2024-05-07 10:56:53 +0200

    Add files via upload

---
+++ dataAcces/SupplierDetailDataBaseAccess.java
@@ -0,0 +1,97 @@
+package dataAccess;
+
+import exception.*;
+import model.SupplierDetail;
+
+import java.sql.*;
+import java.time.LocalDate;
+
+
+public class SupplierDetailDataBaseAccess {
+    //create
+    public void createSupplierDetail(SupplierDetail supplierDetail) throws SupplierDetailException {
+        try {
+            Connection connection = SingletonConnection.getInstance();
+            String query =
+                    """
+                    INSERT INTO supplierDetail
+                    VALUES (?,?,?,?,?,?);
+                    """;
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1,supplierDetail.getProduct());
+            statement.setString(2,supplierDetail.getSupplier());
+            //Trouver solution pour LocalDateTime
+            statement.setLocalDateTime(3,supplierDetail.getDateAndTime());
+            statement.setInt(4,supplierDetail.getQuantity());
+            statement.setDouble(5,supplierDetail.getUnitPrice());
+            Integer minimumQuantity = supplierDetail.getMinimumQuantity();
+            if(minimumQuantity != null){
+                statement.setInt(6,supplierDetail.getMinimumQuantity());
+            } else {
+                statement.setNull(6, Types.NULL);
+            }
+            statement.executeUpdate();
+
+        }
+        catch (SQLException exception) {
+            throw new SupplierDetailException(exception.getMessage(), new OneException(), new CreateException());
+        }
+    }
+    //read
+    public SupplierDetail readSupplierDetail(int productCode, String supplierName, LocalDate dateAndTimeId) throws SupplierDetailException {
+        try {
+            Connection connection = SingletonConnection.getInstance();
+            String query = "SELECT * FROM supplierDetail WHERE product = ? AND supplier=? AND dateAndTime= ?;";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, productCode);
+            statement.setString(2, supplierName);
+            //Trouver solution pour LocalDateTime
+            statement.setLocalDate(3, dateAndTimeId);
+            ResultSet data = statement.executeQuery();
+            data.next();
+            int product = data.getInt("product");
+            String supplier = data.getString("supplier");
+            LocalDateTime dateAndTime = data.getLocalDateTime("dateAndTime");
+            int quantity = data.getInt("quantity");
+            double unitPrice = data.getDouble("unitPrice");
+            Integer minimumQuantity = data.getInt("minimumQuantity");
+            return new SupplierDetail(product, supplier, dateAndTime, quantity, unitPrice, minimumQuantity);
+        } catch (SQLException exception) {
+            throw new SupplierDetailException(exception.getMessage(), new OneException(), new ReadException());
+        }
+    }
+
+    //update
+
+    public void updateSupplierDetail(SupplierDetail supplierDetail) throws SupplierDetailException {
+        try {
+            Connection connection = SingletonConnection.getInstance();
+            String query =
+                    """
+                    UPDATE supplierDetail
+                    SET supplier = ?, dateAndTime = ?, quantity = ?, unitPrice = ?, minimumQuantity = ?
+                    WHERE product = ?;
+                    """;
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1,supplierDetail.getSupplier());
+            //Trouver solution pour LocalDateTime
+            statement.setLocalDateTime(2,supplierDetail.getDateAndTime());
+            statement.setInt(3,supplierDetail.getQuantity());
+            statement.setDouble(4,supplierDetail.getUnitPrice());
+            Integer minimumQuantity = supplierDetail.getMinimumQuantity();
+            if(minimumQuantity != null){
+                statement.setInt(5,supplierDetail.getMinimumQuantity());
+            } else {
+                statement.setNull(5, Types.NULL);
+            }
+            statement.setInt(6,supplierDetail.getProduct());
+            statement.executeUpdate();
+
+        }
+        catch (SQLException exception) {
+            throw new SupplierDetailException(exception.getMessage(), new OneException(), new UpdateException());
+        }
+    }
+
+
+}
