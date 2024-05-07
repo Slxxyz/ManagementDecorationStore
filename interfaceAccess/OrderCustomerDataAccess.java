48e7ee3e411e5ef484cdb42a785fb1070783dc4a
MatteoMameri <160240372+MatteoMameri@users.noreply.github.com>
2024-05-07 13:53:54 +0200

    Update OrderCustomerDataAccess.java

--- interfaceAccess/OrderCustomerDataAccess.java
+++ interfaceAccess/OrderCustomerDataAccess.java
@@ -7,6 +7,16 @@ import java.util.ArrayList;
 
 public class OrderCustomerDataAccess {
 
-
-  
+    void createOrderCustomer(OrderCustomer orderCustomer) throws OrderCustomerException 
+    
+    void deleteOrderCustomer(Integer orderCode) throws OrderCustomerException 
+   
+    void deleteAllOrderCustomer(Integer customerNumber) throws OrderCustomerException 
+   
+    ArrayList<OrderCustomer> readAllOrderCustomer() throws OrderCustomerException 
+   
+    ArrayList<OrderCustomer> readAllOrderCustomerFor(Integer customerNumber) throws OrderCustomerException 
+       
+    OrderCustomer readOrderCustomer(int codeOrderCustomer) throws OrderCustomerException 
+   
 }
