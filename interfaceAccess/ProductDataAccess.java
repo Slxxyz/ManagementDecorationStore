1cdb31663022416e221f0724b02bebf1f5dad834
MatteoMameri <160240372+MatteoMameri@users.noreply.github.com>
2024-05-07 12:06:00 +0200

    Update ProductDataAccess.java

--- interfaceAccess/ProductDataAccess.java
+++ interfaceAccess/ProductDataAccess.java
@@ -6,6 +6,18 @@ import model.Product;
 import java.util.ArrayList;
 
 public class ProductDataAccess {
+
+    void createProduct(Product product) throws ProductException 
+
+    Product readProduct(int codeProduct) throws ProductException 
+
+    void updateProduct(Product product) throws ProductException 
+  
+    void deleteProduct(int codeProduct) throws ProductException 
+
+    ArrayList<Product> readAllProducts() throws ProductException 
+     
+
 }
 
 
