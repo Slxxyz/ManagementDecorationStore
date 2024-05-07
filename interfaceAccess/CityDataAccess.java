ad60bd6af975e671b824e742abb4fb356b3d8f96
MatteoMameri <160240372+MatteoMameri@users.noreply.github.com>
2024-05-07 11:52:59 +0200

    Update CityDataAccess.java

--- interfaceAccess/CityDataAccess.java
+++ interfaceAccess/CityDataAccess.java
@@ -6,4 +6,9 @@ import model.City;
 import java.util.ArrayList;
 
 public class CityDataAccess {
+
+  ArrayList<City> readAllCity() throws CityException
+    
+  City readCity(int id) throws CityException 
+  
 }
