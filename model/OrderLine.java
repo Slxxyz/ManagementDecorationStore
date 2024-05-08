576f6b9122ddaa3be7a17e41ed4548a3cf8f6009
Evren Konakci <142110440+Slxxyz@users.noreply.github.com>
2024-05-08 08:47:45 +0200

    fix : FK_Key

--- model/OrderLine.java
+++ model/OrderLine.java
@@ -1,12 +1,19 @@
 package model;
 
+
+
+
 public class OrderLine {
     private int code;
     private int quantity;
+    private int orderCustomer;
+    private int product;
 
-    public OrderLine(int code, int quantity) {
+    public OrderLine(int code, int quantity, int orderCustomer, int product) {
         this.code = code;
         this.quantity = quantity;
+        this.orderCustomer = orderCustomer;
+        this.product = product;
     }
     //////////////////////////////////////////////////////////////////GETTERS
     public int getCode() {
@@ -17,6 +24,14 @@ public class OrderLine {
         return quantity;
     }
 
+    public int getOrderCustomer() {
+        return orderCustomer;
+    }
+
+    public int getProduct() {
+        return product;
+    }
+
     //////////////////////////////////////////////////////////////////SETTERS
 
     public void setCode(int code) {
@@ -25,5 +40,13 @@ public class OrderLine {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public void setOrderCustomer(int orderCustomer) {
+        this.orderCustomer = orderCustomer;
+    }
+
+    public void setProduct(int product) {
+        this.product = product;
     }
 }
