0772594cc69f7650d5e536d76b6c75b715b9bb67
Evren Konakci <142110440+Slxxyz@users.noreply.github.com>
2024-05-24 17:35:20 +0200

    Add files via upload

--- interfaceAccess/OrderCustomerDataAccess.java
+++ interfaceAccess/OrderCustomerDataAccess.java
@@ -1,6 +1,8 @@
 package interfaceAccess;
 
 import exception.OrderCustomerException;
+import exception.NextCodeOrderCustomerException;
+import exception.NumberOrderCustomerException;
 import model.OrderCustomer;
 import java.util.ArrayList;
 public interface OrderCustomerDataAccess {
@@ -8,6 +10,10 @@ public interface OrderCustomerDataAccess {
     public void createOrderCustomer(OrderCustomer orderCustomer) throws OrderCustomerException;
     //read
     public OrderCustomer readOrderCustomer(int orderCustomerId) throws OrderCustomerException;
+
+    //delete
+    public void deleteOrderCustomer(int orderCustomerId) throws OrderCustomerException;
+
     //delete All Order for a customer
     public void deleteAllOrderCustomerFor(int customerNumber) throws OrderCustomerException;
 
@@ -15,5 +21,9 @@ public interface OrderCustomerDataAccess {
     public ArrayList<OrderCustomer> readAllOrderCustomers() throws OrderCustomerException;
     //readAllOrderCustomerFor
     public ArrayList<OrderCustomer> readAllOrderCustomerFor(int customerNumber) throws OrderCustomerException;
+    //getNextCode
+    public int getNextCode() throws NextCodeOrderCustomerException;
+    //getNumberOrderCustomer
+    public int getNumberOrderCustomer() throws NumberOrderCustomerException;
 
 }
--- interfaceAccess/OrderLineDataAccess.java
+++ interfaceAccess/OrderLineDataAccess.java
@@ -1,6 +1,7 @@
 package interfaceAccess;
 
 import exception.OrderLineException;
+import exception.NextCodeOrderLineException;
 import model.OrderLine;
 import java.util.ArrayList;
 public interface OrderLineDataAccess {
@@ -9,5 +10,11 @@ public interface OrderLineDataAccess {
     //readAll
     public ArrayList<OrderLine> readAllOrderLines() throws OrderLineException;
     //readAllFor
-    public ArrayList<OrderLine> readAllOrderLinesFor(int orderLineCode) throws OrderLineException;
+    public ArrayList<OrderLine> readAllOrderLinesFor(int orderCode) throws OrderLineException;
+    //deleteAllFor
+    public void deleteAllOrderLinesFor(int orderCode) throws OrderLineException;
+    //getNextCode
+    public int getNextCode() throws NextCodeOrderLineException;
+
+
 }
