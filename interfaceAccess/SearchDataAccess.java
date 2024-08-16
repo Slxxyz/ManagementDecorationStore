52bd4a795c8017170b4911f613775e9291278709
MatteoMameri <160240372+MatteoMameri@users.noreply.github.com>
2024-08-16 10:51:12 +0200

    Update SearchDataAccess.java

--- interfaceAccess/SearchDataAccess.java
+++ interfaceAccess/SearchDataAccess.java
@@ -1,4 +1,23 @@
+
 package interfaceAccess;
 
-public class SearchDataAccess {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.*;
+
+import exception.SearchSupplierHistoryException;
+import model.*;
+
+public interface SearchDataAccess {
+
+    ArrayList<SearchSupplierDetailResult> searchSupplierHistory( String supplierLegalName, LocalDate startDate, LocalDate endDate) throws SQLException, SearchSupplierHistoryException;
+
+    Map<Integer, SearchOrderCustomerResult> searchEachOrderOfCustomer(int number, LocalDate startDate, LocalDate endDate) throws SQLException;
+
+    Map<Integer, SearchOrderCustomerResults> searchEachOrderOfProduct(int productCode, LocalDate startDate, LocalDate endDate) throws SQLException;
+
+    List<SalesSearchResult> searchSalesBetweenDates(LocalDate startDate, LocalDate endDate) throws SQLException;
+
 }
