af97e491d48bf63f885ce78ab06ea4b6e5384dd7
MatteoMameri <160240372+MatteoMameri@users.noreply.github.com>
2024-08-16 10:45:41 +0200

    Create SearchOrderLineResults.java

---
+++ model/SearchOrderLineResults.java
@@ -0,0 +1,59 @@
+package model;
+
+
+public class SearchOrderLineResults {
+
+    private int orderLineCode;
+
+    private int quantity;
+    private Float productPrice;
+    private String productLabel;
+    private String productCategory;
+
+
+
+
+    // Constructeur
+    public SearchOrderLineResults(int orderLineCode, int quantity, String productLabel,Float productPrice, String productCategory) {
+        this.orderLineCode = orderLineCode;
+        this.quantity = quantity;
+        this.productLabel = productLabel;
+        this.productPrice = productPrice;
+        this.productCategory = productCategory;
+
+    }
+
+    // Getters
+    public int getOrderLineCode() {
+        return orderLineCode;
+    }
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+
+    public String getProductLabel() {
+        return productLabel;
+    }
+
+    public float getProductPrice(){
+        return productPrice;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
