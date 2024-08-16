e2ef5d1d96659119cf3a3ebb89256f63b659e595
MatteoMameri <160240372+MatteoMameri@users.noreply.github.com>
2024-08-16 10:54:21 +0200

    Create SearchSupplierHistoryException.java

---
+++ exception/SearchSupplierHistoryException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class SearchSupplierHistoryException extends Exception {
+    public SearchSupplierHistoryException(String message) {
+        super(message);
+    }
+
+}
